6305fc3d60591cc20cdc6ca31964cb73be9e03f0
Artur diniz samora <artur.diniz.samora@gmail.com>
2023-09-30 11:33:33 -0300

    ajuste de template

--- src/main/java/com/example/template/security/SecurityConfiguration.java
+++ src/main/java/com/example/template/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs/**","/swagger-ui/**", "/actuator/**").permitAll()
+                .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html/**", "/actuator/**").permitAll()
                 .anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(authenticationEntryPoint);
 
         http.csrf().disable();
