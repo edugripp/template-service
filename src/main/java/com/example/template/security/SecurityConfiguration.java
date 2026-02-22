ffbcaf55ebb8946b516b6e00ac42cae01c8a6b63
Artur Diniz Samora <asamora@firsthelp.com>
2026-02-22 16:52:27 -0300

    update dependencies

--- src/main/java/com/example/template/security/SecurityConfiguration.java
+++ src/main/java/com/example/template/security/SecurityConfiguration.java
@@ -28,11 +28,12 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html/**", "/actuator/**").permitAll()
-                .anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(authenticationEntryPoint);
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/actuator/**").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(basic -> basic.authenticationEntryPoint(authenticationEntryPoint))
+                .csrf(csrf -> csrf.disable());
 
-        http.csrf().disable();
         return http.build();
     }
 
@@ -42,8 +43,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
-    {
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         List<UserDetails> userDetailsList = new ArrayList<>();
         userDetailsList.add(User.withUsername("admin").password(passwordEncoder().encode("admin"))
                 .roles("ADMIN", "USER").build());
@@ -53,12 +53,12 @@ public class SecurityConfiguration {
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http,
-                                                       PasswordEncoder passwordEncoder,
-                                                       InMemoryUserDetailsManager userDetailsManager) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsManager)
-                .passwordEncoder(passwordEncoder)
-                .and()
-                .build();
+            PasswordEncoder passwordEncoder,
+            InMemoryUserDetailsManager userDetailsManager) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsManager)
+                .passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
     }
 }
